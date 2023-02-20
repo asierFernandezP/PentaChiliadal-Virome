2340ded3c91a05ac3e3c753f60daced7037a4970
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-02-20 15:15:31 +0100

    Create CheckV_contamination_completeness.sh

---
+++ Scripts/3_Contamination_and_Completeness_Contigs/CheckV_contamination_completeness.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=CheckV_contigs
+#SBATCH --output=CheckV_contigs.out
+#SBATCH --mem=64gb
+#SBATCH --time=10-0
+#SBATCH --cpus-per-task=24
+#SBATCH --open-mode=truncate
+#SBATCH --partition=regular
+
+contig_file=$1 #path to FASTA file with the predicted viral contigs
+output=$2 #path to output directory
+
+# Clean environment, load modules and activate conda environment
+module purge; ml Anaconda3; module list
+source activate /data/umcg-llnext/python_venvs/CheckV_conda; conda list 
+#Add prodigal-gv to path:
+export PATH="/data/umcg-llnext/python_venvs/CheckV_conda/prodigal-gv:$PATH"
+
+# Run CheckV
+checkv end_to_end $contig_file $output -t ${SLURM_CPUS_PER_TASK} -d /data/umcg-llnext/python_venvs/CheckV_conda/checkv-db-v1.5
+
+conda deactivate
