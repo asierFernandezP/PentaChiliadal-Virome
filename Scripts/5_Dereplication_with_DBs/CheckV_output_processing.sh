2f4c92e47c5d907db08ca3b7bc1b68e2af796735
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-03 11:13:34 +0100

    Update CheckV_output_processing.sh

--- Scripts/5_Dereplication_with_DBs/CheckV_output_processing.sh
+++ Scripts/5_Dereplication_with_DBs/CheckV_output_processing.sh
@@ -14,9 +14,6 @@ cd $CheckV_dir
 # Merge the selected sequences from the proviruses.fna and viruses.fna files
 grep -Fwf selected_CheckV_contigs.txt -A 1 viruses.fna > CheckV_sequences.fna
 sed 's/.*/&_/' selected_CheckV_contigs.txt | grep -A 1 -Ff - proviruses.fna >> CheckV_sequences.fna #adds an underscore at the end of each line. The option - tells grep to read the patterns to search from standard input
-n_final_sequences=$(cat CheckV_sequences.fna| grep ">" | wc -l)
-echo "The final number of viral sequences with completeness >50% is: $n_final_sequences"
-echo -e "The final sequences are available in CheckV_sequences.fna file\n"
 
 # Set permissions
 chmod 440 CheckV_sequences.fna 
