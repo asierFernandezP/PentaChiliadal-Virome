c1af00891ae228b8f0e6d87f0d9c80fe79b55a24
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-03 15:08:53 +0100

    Update CheckV_viral_regions.sh

--- Scripts/5_Dereplication_with_DBs/CheckV_viral_regions.sh
+++ Scripts/5_Dereplication_with_DBs/CheckV_viral_regions.sh
@@ -11,6 +11,10 @@
 CheckV_dir=$1
 cd $CheckV_dir
 
+# Get the names of contigs whose viral regions will be kept (completeness > 50%) or discarded.
+awk 'NR>1' quality_summary.tsv | awk '$8 != "Low-quality" && $8 != "Not-determined"' | cut -f1 | sort > selected_CheckV_contigs.txt
+awk 'NR>1' quality_summary.tsv | awk '$8 == "Low-quality" || $8 == "Not-determined"' | cut -f1 | sort > filtered_CheckV_contigs.txt
+
 # Merge the selected sequences from the proviruses.fna and viruses.fna files
 grep -Fwf selected_CheckV_contigs.txt -A 1 viruses.fna > CheckV_sequences.fna
 sed 's/.*/&_/' selected_CheckV_contigs.txt | grep -A 1 -Ff - proviruses.fna >> CheckV_sequences.fna #adds an underscore at the end of each line. The option - tells grep to read the patterns to search from standard input
