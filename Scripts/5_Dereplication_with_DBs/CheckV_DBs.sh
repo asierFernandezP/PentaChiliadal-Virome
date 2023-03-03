0b9b4627337a508d2aff49d97c05ddbad0e53153
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-03 10:48:47 +0100

    Update and rename CheckV_GPD.sh to CheckV_DBs.sh

--- Scripts/5_Dereplication_with_DBs/CheckV_DBs.sh
+++ Scripts/5_Dereplication_with_DBs/CheckV_DBs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=CheckV_GPD
-#SBATCH --output=CheckV_GPD.out
+#SBATCH --job-name=CheckV_DB
+#SBATCH --output=CheckV__%A_%a.out
 #SBATCH --mem=32gb
 #SBATCH --time=7-0
 #SBATCH --cpus-per-task=8
@@ -8,8 +8,11 @@
 #SBATCH --get-user-env=L
 #SBATCH --partition=regular
 
-database=$1 #path to FASTA file of the database to run through CheckV (e.g. GPD) #/data/umcg-tifn/DATABASES/GPD/GPD_sequences.fa
+database=$1 #path to FASTA file of the database to run through CheckV (e.g. GPD)
+db_name="$(basename "${database}")" #extract filename
 output=$2 #path to output directory (no need to create it in advance)
+
+echo '-------------------- WORKING WITH '${db_name}' DATABASE --------------------'
 
 # Clean environment, load modules and activate conda environment
 module purge; ml Anaconda3; module list
@@ -18,6 +21,11 @@ source activate /data/umcg-llnext/python_venvs/CheckV_conda; conda list
 export PATH="/data/umcg-llnext/python_venvs/CheckV_conda/prodigal-gv:$PATH"
 
 # Run CheckV
-checkv end_to_end $database $output -t ${SLURM_CPUS_PER_TASK} -d /data/umcg-llnext/python_venvs/CheckV_conda/checkv-db-v1.5
+checkv \
+			 end_to_end \
+			 $database \
+			 $output \
+			 -t ${SLURM_CPUS_PER_TASK} \
+			 -d /data/umcg-llnext/python_venvs/CheckV_conda/checkv-db-v1.5
 
 conda deactivate
