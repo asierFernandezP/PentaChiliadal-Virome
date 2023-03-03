ba52f18b30963719fa17b165bca1b7a19273a0e3
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-03 11:41:07 +0100

    Update CheckV_full_contigs.sh

--- Scripts/5_Dereplication_with_DBs/CheckV_full_contigs.sh
+++ Scripts/5_Dereplication_with_DBs/CheckV_full_contigs.sh
@@ -9,14 +9,14 @@
 #SBATCH --partition=short
 
 CheckV_dir=$1
+database=$2
 cd $CheckV_dir
 
-# Select the full contigs with a completeness > 50% estimated by CheckV 
-cat proviruses.fna | grep ">"| cut -f2 -d ">"
-
-
-grep -Fwf selected_CheckV_contigs.txt -A 1 viruses.fna > CheckV_sequences.fna
-sed 's/.*/&_/' selected_CheckV_contigs.txt | grep -A 1 -Ff - proviruses.fna >> CheckV_sequences.fna #adds an underscore at the end of each line. The option - tells grep to read the patterns to search from standard input
+# Extract from the FASTA file of the DB the sequences with a completeness > 50% estimated by CheckV
+seqtk subseq \
+    -l 80 \
+    $database \
+    selected_CheckV_contigs.txt > CheckV_sequences.fna
 
 # Set permissions
 chmod 440 CheckV_sequences.fna 
