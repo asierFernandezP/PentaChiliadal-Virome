0ee41722550be1c5955d0248de87b9a73297e2f2
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-06 10:10:56 +0100

    Update run_all_contig_files_CheckV.sh

--- Scripts/3_Contamination_and_Completeness_Contigs/run_all_contig_files_CheckV.sh
+++ Scripts/3_Contamination_and_Completeness_Contigs/run_all_contig_files_CheckV.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir /scratch/umcg-afernandez/split_CheckV
+
 for FILE in $@; do
   file_name="$(basename "${FILE}")" #extract filename
 	sbatch \
 		--output ./out/${file_name}_CheckV.out \
 		--job-name ${file_name}_CheckV \
-		CheckV_contamination_completeness.sh ${FILE} /scratch/umcg-afernandez/${file_name}_CheckV_result
+		CheckV_contamination_completeness.sh ${FILE} /scratch/umcg-afernandez/split_CheckV/${file_name}_CheckV
 done
