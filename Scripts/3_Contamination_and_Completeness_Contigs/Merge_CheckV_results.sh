1b745563f374ca304ee2fcd491bacd351c6e3166
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-07 03:23:36 +0100

    Update Merge_CheckV_results.sh

--- Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
+++ Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
@@ -8,13 +8,15 @@
 #SBATCH --get-user-env=L
 #SBATCH --partition=short
 
+CheckV_output=$1 # CheckV output folder
+
 # Move all result folders from the 20 partitions to the same directory
 # Concatenate viruses.fna files
-find . -type f -name "viruses.fna" -exec cat {} + >> viruses.fna
+find $CheckV_output -type f -name "viruses.fna" -exec cat {} + >> viruses.fna
 # Concatenate proviruses.fna files
-find . -type f -name "proviruses.fna" -exec cat {} + >> proviruses.fna
+find $CheckV_output -type f -name "proviruses.fna" -exec cat {} + >> proviruses.fna
 # Concatenate quality_summary.tsv, contamination.tsv, completeness.tsv and complete_genomes.tsv
-find . -type f -name "quality_summary.tsv" -exec cat {} + >> quality_summary.tsv
-find . -type f -name "contamination.tsv" -exec cat {} + >> contamination.tsv
-find . -type f -name "completeness.tsv" -exec cat {} + >> completeness.tsv
-find . -type f -name "complete_genomes.tsv" -exec cat {} + >> complete_genomes.tsv
+find $CheckV_output -type f -name "quality_summary.tsv" -exec cat {} + >> quality_summary.tsv
+find $CheckV_output -type f -name "contamination.tsv" -exec cat {} + >> contamination.tsv
+find $CheckV_output -type f -name "completeness.tsv" -exec cat {} + >> completeness.tsv
+find $CheckV_output -type f -name "complete_genomes.tsv" -exec cat {} + >> complete_genomes.tsv
