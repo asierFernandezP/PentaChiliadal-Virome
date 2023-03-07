5e591c409f3d897255c67bcd0adaaed172975f19
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-07 03:19:02 +0100

    Update Merge_CheckV_results.sh

--- Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
+++ Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=merge_CheckV_results
 #SBATCH --output=merge_CheckV.out
 #SBATCH --mem=4gb
-#SBATCH --time=00:19:59
+#SBATCH --time=00:29:59
 #SBATCH --cpus-per-task=4
 #SBATCH --export=NONE
 #SBATCH --get-user-env=L
@@ -13,5 +13,8 @@
 find . -type f -name "viruses.fna" -exec cat {} + >> viruses.fna
 # Concatenate proviruses.fna files
 find . -type f -name "proviruses.fna" -exec cat {} + >> proviruses.fna
-# Concatenate quality_summary.tsv files
+# Concatenate quality_summary.tsv, contamination.tsv, completeness.tsv and complete_genomes.tsv
 find . -type f -name "quality_summary.tsv" -exec cat {} + >> quality_summary.tsv
+find . -type f -name "contamination.tsv" -exec cat {} + >> contamination.tsv
+find . -type f -name "completeness.tsv" -exec cat {} + >> completeness.tsv
+find . -type f -name "complete_genomes.tsv" -exec cat {} + >> complete_genomes.tsv
