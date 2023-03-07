935d0b3a2a457107ab61c8cc6f82778e541b2d58
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-07 15:11:36 +0100

    Update Merge_CheckV_results.sh

--- Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
+++ Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
@@ -12,11 +12,12 @@ CheckV_output=$1 # CheckV output folder
 mkdir CheckV_merged_results # directory to store results
 
 # Concatenate viruses.fna files
-find $CheckV_output -type f -name "viruses.fna" -exec cat {} + >> CheckV_merged_results/viruses.fna
+# Important to use 'NR>1' to avoid concatenating headers
+find $CheckV_output -type f -name "viruses.fna" -exec awk 'NR>1' {} + >> CheckV_merged_results/viruses.fna
 # Concatenate proviruses.fna files
-find $CheckV_output -type f -name "proviruses.fna" -exec cat {} + >> CheckV_merged_results/proviruses.fna
+find $CheckV_output -type f -name "proviruses.fna" -exec awk 'NR>1' {} + >> CheckV_merged_results/proviruses.fna
 # Concatenate quality_summary.tsv, contamination.tsv, completeness.tsv and complete_genomes.tsv
-find $CheckV_output -type f -name "quality_summary.tsv" -exec cat {} + >> CheckV_merged_results/quality_summary.tsv
-find $CheckV_output -type f -name "contamination.tsv" -exec cat {} + >> CheckV_merged_results/contamination.tsv
-find $CheckV_output -type f -name "completeness.tsv" -exec cat {} + >> CheckV_merged_results/completeness.tsv
-find $CheckV_output -type f -name "complete_genomes.tsv" -exec cat {} + >> CheckV_merged_results/complete_genomes.tsv
+find $CheckV_output -type f -name "quality_summary.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/quality_summary.tsv
+find $CheckV_output -type f -name "contamination.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/contamination.tsv
+find $CheckV_output -type f -name "completeness.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/completeness.tsv
+find $CheckV_output -type f -name "complete_genomes.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/complete_genomes.tsv
