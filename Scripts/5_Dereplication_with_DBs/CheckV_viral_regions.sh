735dd985c975f906d86c641cfbcf35737605d823
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-07 04:10:31 +0100

    Update CheckV_viral_regions.sh

--- Scripts/5_Dereplication_with_DBs/CheckV_viral_regions.sh
+++ Scripts/5_Dereplication_with_DBs/CheckV_viral_regions.sh
@@ -15,9 +15,15 @@ cd $CheckV_dir
 awk 'NR>1' quality_summary.tsv | awk '$8 != "Low-quality" && $8 != "Not-determined"' | cut -f1 | sort > selected_CheckV_contigs.txt
 awk 'NR>1' quality_summary.tsv | awk '$8 == "Low-quality" || $8 == "Not-determined"' | cut -f1 | sort > filtered_CheckV_contigs.txt
 
-# Merge the selected sequences from the proviruses.fna and viruses.fna files
-grep -Fwf selected_CheckV_contigs.txt -A 1 viruses.fna > CheckV_sequences.fna
-sed 's/.*/&_/' selected_CheckV_contigs.txt | grep -A 1 -Ff - proviruses.fna >> CheckV_sequences.fna #adds an underscore at the end of each line. The option - tells grep to read the patterns to search from standard input
-
+# Extract from the viruses.fna and proviruses.fna files the sequences with a completeness > 50% estimated by CheckV
+seqtk subseq \
+    -l 80 \
+    viruses.fna \
+    selected_CheckV_contigs.txt > CheckV_sequences.fna
+seqtk subseq \
+    -l 80 \
+    proviruses.fna \
+    selected_CheckV_contigs.txt >> CheckV_sequences.fna    
+    
 # Set permissions
 chmod 440 CheckV_sequences.fna 
