aaceb7e93ac7966244dbbe74642dc18fa17a891a
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-03-07 16:15:24 +0100

    Update Merge_CheckV_results.sh

--- Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
+++ Scripts/3_Contamination_and_Completeness_Contigs/Merge_CheckV_results.sh
@@ -19,10 +19,10 @@ find $CheckV_output -type f -name "proviruses.fna" -exec cat {} + >> CheckV_merg
 # Concatenate quality_summary.tsv, contamination.tsv, completeness.tsv and complete_genomes.tsv
 # Important to use 'NR>1' to avoid concatenating headers
 find $CheckV_output -name "quality_summary.tsv" -type f -exec head -1 {} \; -quit > CheckV_merged_results/quality_summary.tsv
-find $CheckV_output -type f -name "quality_summary.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/quality_summary.tsv
+find $CheckV_output -type f -name "quality_summary.tsv" -exec sed -e 1d '{}' \; >> CheckV_merged_results/quality_summary.tsv
 find $CheckV_output -name "contamination.tsv" -type f -exec head -1 {} \; -quit > CheckV_merged_results/contamination.tsv
-find $CheckV_output -type f -name "contamination.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/contamination.tsv
+find $CheckV_output -type f -name "contamination.tsv" -exec sed -e 1d '{}' \; >> CheckV_merged_results/contamination.tsv
 find $CheckV_output -name "completeness.tsv" -type f -exec head -1 {} \; -quit > CheckV_merged_results/completeness.tsv
-find $CheckV_output -type f -name "completeness.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/completeness.tsv
+find $CheckV_output -type f -name "completeness.tsv" -exec sed -e 1d '{}' \; >> CheckV_merged_results/completeness.tsv
 find $CheckV_output -name "complete_genomes.tsv" -type f -exec head -1 {} \; -quit > CheckV_merged_results/complete_genomes.tsv
-find $CheckV_output -type f -name "complete_genomes.tsv" -exec awk 'NR>1' {} + >> CheckV_merged_results/complete_genomes.tsv
+find $CheckV_output -type f -name "complete_genomes.tsv" -exec sed -e 1d '{}' \; >> CheckV_merged_results/complete_genomes.tsv
