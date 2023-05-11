a9d80c15e6368a73a637c161473aee17764272da
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-05-11 09:53:38 +0200

    Update CheckV_DBs.sh

--- Scripts/5_Dereplication_with_DBs/CheckV_DBs.sh
+++ Scripts/5_Dereplication_with_DBs/CheckV_DBs.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=CheckV_DB
 #SBATCH --output=CheckV_%j.out
 #SBATCH --mem=32gb
-#SBATCH --time=7-0
-#SBATCH --cpus-per-task=8
+#SBATCH --time=4-0
+#SBATCH --cpus-per-task=16
 #SBATCH --export=NONE
 #SBATCH --get-user-env=L
-#SBATCH --partition=regular
+#SBATCH --partition=himem
 
 database=$1 #path to FASTA file of the database to run through CheckV (e.g. GPD)
 db_name="$(basename "${database}")" #extract filename
@@ -16,9 +16,7 @@ echo '-------------------- WORKING WITH '${db_name}' DATABASE ------------------
 
 # Clean environment, load modules and activate conda environment
 module purge; ml Anaconda3; module list
-source activate /data/umcg-llnext/python_venvs/CheckV_conda; conda list 
-#Add prodigal-gv to path:
-export PATH="/data/umcg-llnext/python_venvs/CheckV_conda/prodigal-gv:$PATH"
+source activate /home2/p304845/Conda_envs/CheckV_conda_env; conda list
 
 # Run CheckV
 checkv \
@@ -26,6 +24,6 @@ checkv \
 	$database \
 	$output \
 	-t ${SLURM_CPUS_PER_TASK} \
-	-d /data/umcg-llnext/python_venvs/CheckV_conda/checkv-db-v1.5
+	-d /home2/p304845/Conda_envs/CheckV_conda_env/checkv-db-v1.5
 
 conda deactivate
