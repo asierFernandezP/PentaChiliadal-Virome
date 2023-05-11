a7a4614edc865b7e8afddf9fa70d518bf06de664
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-05-11 15:50:12 +0200

    Update Viral_RefSeq_processing.sh

--- Scripts/5_Dereplication_with_DBs/Viral_RefSeq_processing.sh
+++ Scripts/5_Dereplication_with_DBs/Viral_RefSeq_processing.sh
@@ -11,8 +11,9 @@
 database=$1 #path to FASTA file of the database to run through CheckV (Viral_RefSeq)
 database_tax=$2 #path to database taxonomy file
 
-# Clean environment and load seqtk module
-module purge; ml seqtk; ml list
+# Clean environment, load modules and activate conda environment
+module purge; ml Anaconda3; module list
+source activate /home2/p304845/Conda_envs/Seqtk_conda/; conda list
 
 # Create a file excluding the names of RNA viruses (Riboviria realm) from Viral RefSeq DB
 awk 'NR>1' $database_tax | grep -v Riboviria | cut -f1,2 | tr "\t" " " | sed 's/.$//'> non_RNA_viral_refseq.txt
