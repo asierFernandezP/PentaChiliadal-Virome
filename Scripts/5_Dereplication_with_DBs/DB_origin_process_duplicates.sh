5385afa085215b11469dfb762d4470245ba9b07d
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-05-12 16:21:43 +0200

    Update DB_origin_process_duplicates.sh

--- Scripts/5_Dereplication_with_DBs/DB_origin_process_duplicates.sh
+++ Scripts/5_Dereplication_with_DBs/DB_origin_process_duplicates.sh
@@ -12,7 +12,7 @@ viral_fasta_dir=$1 # directory with all the FASTA files to be used as input for 
 
 # Clean environment, load modules and activate conda environment
 module purge; ml Anaconda3; ml R; module list
-source activate /home2/p304845/Conda_envs/Seqtk_conda/; conda list
+source activate /home2/p304845/Conda_envs/Seqkit_conda_env/; conda list
 
 # Concatenate all the viral sequences to be used for dereplication
 cat $viral_fasta_dir/*fna > STEP5_input_sequences.fa
@@ -20,14 +20,10 @@ cat $viral_fasta_dir/*fa  >> STEP5_input_sequences.fa
 cat $viral_fasta_dir/*fasta >> STEP5_input_sequences.fa
 
 # Extract the IDs of all the viral sequences to be used for dereplication
-seqtk seq -n STEP5_input_sequences.fa > STEP5_input_sequences.txt
+seqkit seq -n STEP5_input_sequences.fa > STEP5_input_sequences.txt
 
 # Execute the R script 
 Rscript DB_origin_check_duplicated_sequences.R STEP5_input_sequences.txt
-
-# Clean environment, load modules and activate conda environment
-module purge; ml Anaconda3
-source activate /home2/p304845/Conda_envs/Seqkit_conda_env/; conda list
 
 # Remove duplicated sequences
 seqkit rmdup -n < STEP5_input_sequences.fa > STEP5_combined_sequences_nodup.fa
