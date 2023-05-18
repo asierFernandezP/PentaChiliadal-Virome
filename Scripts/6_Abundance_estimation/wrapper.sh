b5e35fc023ddffe2c22dba117df86f83d8c63d4f
asierFernandezP <78979132+asierFernandezP@users.noreply.github.com>
2023-05-18 23:29:44 +0200

    Update wrapper.sh

--- Scripts/6_Abundance_estimation/wrapper.sh
+++ Scripts/6_Abundance_estimation/wrapper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for FILE in $@; do
-  file_name="$(basename "${FILE}")" #extract filename
-	sbatch \
-		--output ${file_name}_CheckV.out \
-		--job-name ${file_name}_CheckV \
-		Read_mapping_BED.sh ${FILE} Bowtie2_index rep_seqs_vOTUs.fa
+BATCH="$1"  # Get the Batch 
+
+for FILE in "${@:2}"; do
+  file_name="$(basename "${FILE}")" # extract filename
+  sbatch \
+    --output ${file_name}_CheckV.out \
+    --job-name ${file_name}_CheckV \
+    Read_mapping_BED.sh ${FILE} ${BATCH} Bowtie2_index rep_seqs_vOTUs.fa 
 done
